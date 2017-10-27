982f23cf901ffd31e6ff0e07974dfcdfdb73a297
Feiqi Zhang <feiqi.zhang@autodesk.com>
2017-10-27 10:52:19 +0800

    use the key words to find the targets.  Extract the url from the targets, and open them in the default browser.

--- crawler-core.py
+++ crawler-core.py
@@ -2,13 +2,26 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+import urllib
+import webbrowser
+
 url = "http://www.leiphone.com"
 
 page = requests.get(url)
 
 soup = BeautifulSoup(page.text,"lxml")
 
-find_target = "3D"
-find = soup.find(text=re.compile(find_target))
+find_target1 = "3D"
+find_target2 = "三维"
 
-print(find)
+find1 = soup.find_all('a', text=re.compile(find_target1))
+find2 = soup.find_all('a', text=re.compile(find_target2))
+
+#print(find)
+
+for link in find1:
+    webbrowser.open_new_tab(link.get('href'))
+
+for link in find2:
+    webbrowser.open_new_tab(link.get('href'))
+
