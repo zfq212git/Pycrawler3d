4e7f0ccb6d59f2200deff939c312a8aa94a78f21
Feiqi Zhang <feiqi.zhang@autodesk.com>
2017-10-27 17:19:10 +0800

    ttt

--- crawler-core.py
+++ crawler-core.py
@@ -5,23 +5,35 @@ from bs4 import BeautifulSoup
 import urllib
 import webbrowser
 
-url = "http://www.leiphone.com"
+url_list = []
+key_word_list = []
+data_file = open("data.txt", "r")
 
-page = requests.get(url)
+break_line = 0
 
-soup = BeautifulSoup(page.text,"lxml")
+for line in data_file:
+	if(break_line ==0 and line[0] == "*"):
+		break_line = 1
 
-find_target1 = "3D"
-find_target2 = "三维"
+	if (break_line == 0 and line[0] != "*"):
+		url_list.append(line)
+	
+	if (break_line == 1 and line[0] != "*"):
+		key_word_list.append(line)
 
-find1 = soup.find_all('a', text=re.compile(find_target1))
-find2 = soup.find_all('a', text=re.compile(find_target2))
+	
 
-#print(find)
 
-for link in find1:
-    webbrowser.open_new_tab(link.get('href'))
 
-for link in find2:
-    webbrowser.open_new_tab(link.get('href'))
+for url in url_list:
+	print(url)
+	page = requests.get(re.compile(url))
+	soup = BeautifulSoup(page.text,"lxml")
+	#print(soup)
+	for key_word in key_word_list:
+		print(key_word)
+		find = soup.find_all('a', text=re.compile(key_word))
+		print(find)
+		for link in find:
+			webbrowser.open_new_tab(link.get('href'))
 
